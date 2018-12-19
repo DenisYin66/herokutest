8b69b8de789bf5f9cc34d2b512b32c4bbc7e2607
尹志诚 <yinzhicheng@cug.edu.cn>
2018-12-19 10:46:21 +0800

    Initial commit

---
+++ src/main/java/com/example/herokutest/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.herokutest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String sayHello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/herokutest/HerokutestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.herokutest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokutestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
